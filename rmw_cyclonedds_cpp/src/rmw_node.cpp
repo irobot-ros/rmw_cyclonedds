d79c1592f8a9dc3a94def53770888e8e5643cc16
Mauro Passerino <mpasserino@irobot.com>
2021-02-15 15:17:06 -0300

    use void * to pass executor ptr

--- rmw_cyclonedds_cpp/src/rmw_node.cpp
+++ rmw_cyclonedds_cpp/src/rmw_node.cpp
@@ -346,7 +346,7 @@ struct user_callback_data_t
 {
   rmw_listener_callback_t callback;
   rmw_listener_event_type_t event_type;
-  const void * user_data;
+  void * user_data;
   const void * entity_handle;
 };
 
@@ -488,7 +488,7 @@ static void dds_listener_callback(dds_entity_t entity, void * arg)
 extern "C" rmw_ret_t rmw_subscription_set_listener_callback(
   rmw_subscription_t * rmw_subscription,
   rmw_listener_callback_t callback,
-  const void * user_data,
+  void * user_data,
   const void * subscription_handle)
 {
   auto sub = static_cast<CddsSubscription *>(rmw_subscription->data);
@@ -516,7 +516,7 @@ extern "C" rmw_ret_t rmw_subscription_set_listener_callback(
 extern "C" rmw_ret_t rmw_service_set_listener_callback(
   rmw_service_t * rmw_service,
   rmw_listener_callback_t callback,
-  const void * user_data,
+  void * user_data,
   const void * service_handle)
 {
   auto srv = static_cast<CddsService *>(rmw_service->data);
@@ -545,7 +545,7 @@ extern "C" rmw_ret_t rmw_service_set_listener_callback(
 extern "C" rmw_ret_t rmw_client_set_listener_callback(
   rmw_client_t * rmw_client,
   rmw_listener_callback_t callback,
-  const void * user_data,
+  void * user_data,
   const void * client_handle)
 {
   auto cli = static_cast<CddsClient *>(rmw_client->data);
@@ -573,7 +573,7 @@ extern "C" rmw_ret_t rmw_client_set_listener_callback(
 extern "C" rmw_ret_t rmw_guard_condition_set_listener_callback(
   rmw_guard_condition_t * rmw_guard_condition,
   rmw_listener_callback_t callback,
-  const void * user_data,
+  void * user_data,
   const void * guard_condition_handle,
   bool use_previous_events)
 {
@@ -602,7 +602,7 @@ extern "C" rmw_ret_t rmw_guard_condition_set_listener_callback(
 extern "C" rmw_ret_t rmw_event_set_listener_callback(
   rmw_event_t * rmw_event,
   rmw_listener_callback_t callback,
-  const void * user_data,
+  void * user_data,
   const void * waitable_handle,
   bool use_previous_events)
 {
